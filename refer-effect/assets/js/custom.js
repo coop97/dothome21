76606849410cf010574c4d2cce5fc416c76f46a0
coop97 <87642226+coop97@users.noreply.github.com>
2021-09-07 14:21:51 +0900

    2021.9.7

--- refer-effect/assets/js/custom.js
+++ refer-effect/assets/js/custom.js
@@ -63,27 +63,43 @@ function agentCheck(){
             });
     });       
 
-    //modal
+    
+
+
+//modal
 
     // $(".bottom button").click(function(){
     //     $("#modal").removeClass().addClass("show");      //removeClass를 실행하고, addClass를 실행하라, 콜백함수처럼 반복호출 되는 것
     // });
 
+    //필요한 곳에만 넣기 위해 함수화
+    // function modal(){       
+    //     document.querySelector(".bottom button").addEventListener("click", function(){
+    //         //document.querySelector("#modal").className = "";      //className을 초기화 시킴
+    //         document.querySelector("#modal").classList.add("show");
+    //         document.querySelector("#modal").classList.remove("hide");
+    //     });
+
+    //     document.querySelector(".modal-cont button").addEventListener("click", function(){
+    //         document.querySelector("#modal").classList.add("hide");
+    //     });
+    // }
+    
+
+    // $(".modal-cont button").click(function(){
+    //     $("#modal").addClass("hide");
+    // });
     document.querySelector(".bottom button").addEventListener("click", function(){
         //document.querySelector("#modal").className = "";      //className을 초기화 시킴
         document.querySelector("#modal").classList.add("show");
         document.querySelector("#modal").classList.remove("hide");
     });
-
-    // $(".modal-cont button").click(function(){
-    //     $("#modal").addClass("hide");
-    // });
-
-    document.querySelector(".modal-cont button").addEventListener("click", function(){
-        document.querySelector("#modal").classList.add("hide");
-    });
+    
 
     // document.querySelector(".modal-cont button").addEventListener("click",()=>{
     //     document.querySelector("#modal").classList.add("hide");
     // });
+    document.querySelector(".modal-cont button").addEventListener("click", function(){
+        document.querySelector("#modal").classList.add("hide");
+    });
 }
