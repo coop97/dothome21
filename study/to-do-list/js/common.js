b69c32f076495a12905f473cffe47ddd8ec071f7
coop97 <87642226+coop97@users.noreply.github.com>
2022-04-25 00:40:41 +0900

    2022.04.25

---
+++ study/to-do-list/js/common.js
@@ -0,0 +1,121 @@
+let myToDoLists =  new Array();
+document.addEventListener("DOMContentLoaded", function(){
+
+    init();
+});
+
+
+
+function init() {
+    // localstorage 여부 확인
+    // 존재 : 리스트 출력
+    // 비존재 : addToDoBox() 호출
+
+    if(localStorage.getItem('myToDoLists')){
+        myToDoLists = searchLocal();
+        drawLocal();
+    } else {
+        addToDoBox();
+        addLocal();
+    }
+}
+
+
+//localstorage 그리기
+function drawLocal() {
+    if(myToDoLists.length == 0){
+        addToDoBox();
+    } else {
+        const box = document.querySelector('.box');
+        if(box.querySelector('label') != null){
+            box.innerHTML = '';
+        }
+        myToDoLists.forEach((myToDo, index) => {
+            const label = document.createElement('label');
+            let isComplete = 0;
+
+            myToDo.toDo.forEach((el)=>{
+                if(el.complete === false){
+                    isComplete++;
+                }
+            })
+
+            label.classList.add('boxLabel');
+            label.htmlFor = `box${index}`;
+            let txt = `
+                <input type="checkbox" id="box${index}" class="checkBoxs" disabled>
+                <div class="box">
+                    <div class="title">
+                        <div class="in">
+                            <div class="tit">${myToDo.title}</div>
+                            <span class="date">${myToDo.date} </span>
+                            <span class="listCount">할 일이 ${isComplete}개 남았습니다.</span>
+                        </div>
+                    </div>
+                    <div class="toDoLists">
+                        <div class="in">
+                            <ul>`;
+                            myToDo.toDo.forEach((el,idx)=>{
+                                txt += el.complete === true ? 
+                                `<li><input type="checkbox" name="" id="box${index}_list${idx+1}" checked /><label for="box${index}_list${idx+1}">${el.text}</label><span class="delListBtn"></span></li>` :
+                                `<li><input type="checkbox" name="" id="box${index}_list${idx+1}" /><label for="box${index}_list${idx+1}">${el.text}</label><span class="delListBtn"></span></li>`;
+                            });                            
+            txt += `</ul>
+                        </div>
+                    </div>
+                    <div class="addToDoArea">
+                        <div class="in">
+                            <input class="newList" type="text" placeholder="할 일을 입력 후, Enter를 누르세요" onkeyup="enterCheck();" />
+                        </div>
+                    </div>
+                    <button class="toDoBtn addToDo" onclick="javascript:toDoBtnClick(this);" type="button"></button>
+                </div>
+            `;
+
+            label.innerHTML = txt;
+            document.querySelector('.box').appendChild(label);
+        });
+    }
+}
+
+//localstorage 조회
+function searchLocal() {
+    let jsonData = JSON.parse(localStorage.getItem('myToDoLists'));
+
+    return jsonData;
+}
+
+//localstorage 생성
+function addLocal() {
+    if(myToDoLists.length == 0){
+        myToDoLists = [];
+    }
+    localStorage.setItem("myToDoLists",JSON.stringify(myToDoLists));
+}
+
+//localstorage 할 일 박스 생성 
+function addLocalToDoBox(title, date) {
+    const toDoBox = {"date": date, "title": title, "toDo": []};
+    myToDoLists.push(toDoBox);
+    addLocal();
+    drawLocal();
+}
+
+//localstorage 할 일 생성
+function addLocalToDo(text, no) {
+    console.log(text, no);
+    const toDo = {"text": text, "complete": false};
+    myToDoLists[no].toDo.push(toDo);
+    addLocal();
+    drawLocal();
+}
+
+//localstorage 삭제
+function delLocal() {
+    
+}
+
+//localstorage 수정
+function modLocal() {
+    
+}
---
+++ study/to-do-list/js/style_common.js
@@ -0,0 +1,141 @@
+// const addBtn = document.querySelector('.addBtn');
+// const delBtn = document.querySelector('.delBtn');
+// const toDoBtns = document.querySelectorAll('.toDoBtn');
+
+//events
+// addBtn.addEventListener('click', addToDoBox);
+// delBtn.addEventListener('click', deleteToDoBox);
+
+// //변수
+// let isNewBox = false;
+
+//function
+// function addToDoBox() {
+//     const dayList = ['일', '월', '화', '수', '목', '금', '토'];
+//     const date = new Date();
+//     const boxNo = myToDoLists.length == 0 ? 0 : myToDoLists.length;
+
+//     let txt = 
+//     `<input type="checkbox" id="box${boxNo}" class="checkBoxs" disabled>
+//     <div class="box">
+//         <div class="title">
+//             <div class="in">
+//                 <input class="toDoListTit" type="text" placeholder="제목을 입력해 주세요." onkeyup="enterCheck();" autofocus />
+//                 <span class="date">${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ${dayList[date.getDay()]}요일 </span>
+//                 <span class="listCount">할 일이 <i>0</i>개 남았습니다.</span>
+//             </div>
+//         </div>
+//         <div class="toDoLists">
+//             <div class="in">
+//                 <ul>
+//                 </ul>
+//             </div>
+//         </div>
+//         <div class="addToDoArea">
+//             <div class="in">
+//                 <input class="newList" type="text" placeholder="할 일을 입력 후, Enter를 누르세요" onkeyup="enterCheck();" />
+//             </div>
+//         </div>
+//         <button class="toDoBtn addToDo" onclick="javascript:toDoBtnClick(this);" type="button"></button>
+//     </div>`;
+
+    
+//     if (!isNewBox) {
+//         const label = document.createElement('label');
+//         label.classList.add('boxLabel');
+//         label.htmlFor = 'box'+myToDoLists.length;
+//         label.innerHTML =txt;
+//         document.querySelector('.box').appendChild(label);
+//     } else {
+//         alert('이미 새로운 To Do List가 존재합니다.\n제목을 입력 후 Enter를 눌러주세요.')
+//     }
+
+//     isNewBox = true;
+// }
+
+//하단 버튼 클릭 시 인풋 나옴
+function toDoBtnClick(btn) {
+    const $toDoBtn = btn;
+    $toDoBtn.classList.toggle('addToDo');
+    $toDoBtn.classList.toggle('exitToDo');
+    if(!$toDoBtn.classList.contains('addToDo')){
+        $toDoBtn.previousElementSibling.style.display = 'block';
+    } else {
+        $toDoBtn.previousElementSibling.style.display = 'none';
+    }
+}
+
+//인풋에 입력 시 리스트에 등록
+function enterCheck() {
+    const target = window.event.target;
+    const keyCode = window.event.keyCode;
+
+    // to do list 박스 추가시 타이틀 입력
+    if( keyCode == 13 && target.classList.contains('toDoListTit')){
+        const div = document.createElement('div');
+        div.classList.add('tit');
+        div.innerText = target.value;
+        target.before(div);
+        addLocalToDoBox(target.value, target.nextElementSibling.innerText);
+        target.remove();
+        isNewBox = false;
+    }
+
+    // to do list의 새로운 할 일 입력시
+    if( keyCode == 13 && target.classList.contains('newList')){
+        const text = target.value;
+        const no = target.closest('.boxLabel').htmlFor.slice(-1);
+
+        addLocalToDo(text, no);
+    }
+}
+
+// function deleteToDoBox() {
+//     let isChk = false;
+//     const checkBoxs = document.querySelectorAll('.checkBoxs');
+//     // active가 없을 때
+//     // 1. active 추가
+//     // 2. list 선택 가능
+//     // active가 있을 때
+//     // 1. 모달창 뜸
+//     // 1-1. 예 : 선택된 모달창 삭제
+//     // 1-2. 아니오 : 선택된 모달창 해제
+//     // 3. active 제거
+
+//     if(!delBtn.classList.contains('active')){
+//         delBtn.classList.add('active');
+//         checkBoxs.forEach(function (checkBox) {
+//             checkBox.disabled = false;
+//         });
+//     } else {
+
+//         for(var i=0;i<checkBoxs.length;i++){
+//             if(checkBoxs[i].checked == true){
+//                 isChk = true;
+//                 break;
+//             }
+//         }
+
+//         if(isChk) {
+//             layerOpen('#delModal');
+//         } else {
+//             checkBoxs.forEach(function (checkBox) {
+//                 checkBox.disabled = true;
+//             });
+            
+//             delBtn.classList.remove('active');
+//         }
+//     }
+// }
+
+// function modalNoBtn() {
+//     const checkBoxs = document.querySelectorAll('.checkBoxs');
+
+//     checkBoxs.forEach(function (checkBox) {
+//         checkBox.checked = false;
+//         checkBox.disabled = true;
+//     });
+    
+//     delBtn.classList.remove('active');
+//     layerClose('#delModal');
+// }
